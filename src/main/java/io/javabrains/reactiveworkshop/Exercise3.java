7e8a8c12dc28a4d919fb9271fb878e8338c0bd94
Sumit Kumar Roy <roysumit949@gmail.com>
2023-12-03 10:22:21 +0530

    Exercise 3 blocking call to get stream of elements

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
@@ -8,10 +9,12 @@ public class Exercise3 {
 
         // Use ReactiveSources.intNumbersFlux()
 
-        // Get all numbers in the ReactiveSources.intNumbersFlux stream
+        System.out.println("Get all numbers in the ReactiveSources.intNumbersFlux stream");
         // into a List and print the list and its size
         // TODO: Write code here
-
+        List<Integer> lt = ReactiveSources.intNumbersFlux().toStream().toList();
+        System.out.println("list is : " + lt);
+        System.out.println("size : " + lt.size());
 
         System.out.println("Press a key to end");
         System.in.read();
